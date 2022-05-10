89ec6fc298f334f4fc4fcd2a70ef77915ad75ab6
Martin Talha <mart97@live.no>
2022-05-10 13:45:52 +0200

    Vehicle Controller V1 + Folder Structure

---
+++ Assets/Scripts/Controller_Vehicle.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller_Vehicle : MonoBehaviour
+{
+    [Header("Attributes")]
+    public float speed_Base_kmh = 6;
+    public float acceleration = 5;
+
+    public float speed_Modifier = 1;
+
+
+    [Header("Read-Only")]
+    public Vector3 velocity;
+    public string currentSpeed;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float timeStep = Time.deltaTime;
+
+        Move(timeStep);
+
+        transform.position += velocity * timeStep;
+    }
+
+				void Move(float timeStep)
+				{
+        int forwardModifier = (Input.GetKey(KeyCode.W) ? 1 : 0) + (Input.GetKey(KeyCode.S) ? -1 : 0);
+        int sidewayModifier = (Input.GetKey(KeyCode.D) ? 1 : 0) + (Input.GetKey(KeyCode.A) ? -1 : 0);
+
+        float speed_Base = speed_Base_kmh / 6 / 6 * 10;
+
+        Vector3 newVelocity = (transform.forward * forwardModifier + transform.right * sidewayModifier).normalized * speed_Base * speed_Modifier;
+
+        float frictionStep = acceleration * timeStep;
+
+        velocity -= velocity * frictionStep;
+        velocity += newVelocity * frictionStep;
+
+								#region current Speed string
+								float speed = Mathf.Floor(velocity.magnitude * 100) / 100;
+        float speed_kmh = Mathf.Round(speed * 60 * 60 / 1000);
+
+        currentSpeed = "Current speed is: " + speed + " m/s. (" + speed_kmh + " km/h)";
+								#endregion
+				}
+}
