c34821557f4401c23ee494e2532343c51ce0638e
Sander <SaWIEn99@outlook.com>
2022-05-23 14:27:35 +0200

    Hazzards rototyyping
    Added random hazard location, and oil spill

---
+++ Assets/Scripts/Hazzards/OilSpill.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OilSpill : MonoBehaviour
+{
+    public float tractionLoss, stearingLoss;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name + "entered");
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.GetComponent<Controller_Vehicle>().friction_Modifier -= tractionLoss;
+            other.GetComponent<Controller_Vehicle>().turnSpeed -= stearingLoss;
+
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log(other.name + "exited");
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.GetComponent<Controller_Vehicle>().friction_Modifier += tractionLoss;
+            other.GetComponent<Controller_Vehicle>().turnSpeed += stearingLoss;
+        }
+    }
+}
---
+++ Assets/Scripts/Hazzards/RndHazardLocation.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RndHazardLocation : MonoBehaviour
+{
+    public Vector3 rayOrigin;
+
+    public float rayMaxDist, radius;
+    public int rayShots;
+    public List<Vector3> rayHits;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            HazardLocations(rayOrigin);
+        }
+    }
+
+    List<Vector3> HazardLocations(Vector3 rayOrigin)
+    {
+        rayHits = new List<Vector3>();
+        while (rayHits.Count < rayShots)
+        {
+            RaycastHit hit;
+            Vector3 ranLoc = new Vector3(Random.Range(-radius, radius), 0, Random.Range(-radius, radius));
+            ranLoc += rayOrigin;
+
+            if (Physics.Raycast(ranLoc, Vector3.down, out hit, rayMaxDist))
+            {
+                //check for valid hit
+                rayHits.Add(hit.point);
+            }
+        }
+        return rayHits;
+
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        foreach (Vector3 item in rayHits)
+        {
+            Gizmos.DrawCube(item, Vector3.one * 5);
+        }
+    }
+}
