f5d538e305f60f93788670257746b8a92565943e
Sander Endresen <SaWIEn99@outlook.com>
2022-05-31 14:49:06 +0200

    Added main menu
    simple main menu, with fuction for quiting and loading deciered scene

---
+++ Assets/Scripts/UI Scripts/Menue_Builder_Navigation.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menue_Builder_Navigation : MonoBehaviour
+{
+    
+    private void Update()
+    {
+        
+    }
+}
