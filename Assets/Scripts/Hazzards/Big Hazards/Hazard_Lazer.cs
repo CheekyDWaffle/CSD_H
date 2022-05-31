4511a05794146c64151c82d0f4bb416ea9390c66
Sander <SaWIEn99@outlook.com>
2022-06-01 01:27:11 +0200

    Builder UI prototype

--- Assets/Scripts/Hazzards/Big Hazards/Hazard_Lazer.cs
+++ Assets/Scripts/Hazzards/Big Hazards/Hazard_Lazer.cs
@@ -8,6 +8,7 @@ public class Hazard_Lazer : MonoBehaviour
     public float lazerLenght;
     public float scaleFactor;
     public float warningTime;
+    public float coolDown;
 
     [Header("Must be assigned")]
     public GameObject lazerPrefab;
@@ -16,18 +17,24 @@ public class Hazard_Lazer : MonoBehaviour
     [HideInInspector]
     public bool fireLazer;
     [HideInInspector]
-    bool retractLazer;
+    public bool isActive;
     [HideInInspector]
     public Vector3 originalTransform;
     [HideInInspector]
     public Vector3 orignalScale;
-    
+
+
+    bool retractLazer;
+    bool drawRay = true;
     private void Start()
     {
+        drawRay = false;
+        fireLazer = true;
+        lazerLenght = lazerLenght * 100 + 50;
         originalTransform = lazerPrefab.transform.position;
         orignalScale = lazerPrefab.transform.localScale;
         float loopBreak = 1000;
-        while (warningPrefab.transform.lossyScale.z <= lazerLenght*2)
+        while (warningPrefab.transform.lossyScale.z <= lazerLenght * 2)
         {
             loopBreak--;
             warningPrefab.transform.localScale += new Vector3(0, 0, 1);
@@ -39,54 +46,69 @@ public class Hazard_Lazer : MonoBehaviour
         }
     }
 
-    private void Update()
+    //delay between shots in seconds
+    IEnumerator fireCooldown()
     {
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            StartCoroutine("fireWarning");
-        }
-
-        if (fireLazer && !retractLazer)
-        {
-            lazerPrefab.transform.localScale += new Vector3(0, scaleFactor * Time.deltaTime, 0);
-            lazerPrefab.transform.localPosition += new Vector3(0, 0, scaleFactor * Time.deltaTime);
-
-            if (lazerPrefab.transform.lossyScale.y >= lazerLenght)
-            {
-                retractLazer = true;
-                warningPrefab.SetActive(false);
-            }
-        }
-        if (retractLazer)
-        {
-            lazerPrefab.transform.localScale -= new Vector3(0, (scaleFactor * 2) * Time.deltaTime, 0);
-            lazerPrefab.transform.localPosition += new Vector3(0, 0, (scaleFactor * 2) * Time.deltaTime);
-
-            if (lazerPrefab.transform.lossyScale.y <= 0)
-            {
-                disepateLazer();
-            }
-        }
-
+        yield return new WaitForSeconds(coolDown);
+        StartCoroutine("fireWarning");
     }
-
-    void disepateLazer()
-    {
-        lazerPrefab.transform.position = originalTransform;
-        lazerPrefab.transform.localScale = orignalScale;
-        fireLazer = false;
-        retractLazer = false;
-    }
+    //How long the warning is visable before the shot is fired
     IEnumerator fireWarning()
     {
         warningPrefab.SetActive(true);
         yield return new WaitForSeconds(warningTime);
         fireLazer = true;
     }
+    //Retracts lazer, resets transform and restarts fire prosses
+    void disepateLazer()
+    {
+        lazerPrefab.transform.position = originalTransform;
+        lazerPrefab.transform.localScale = orignalScale;
+        fireLazer = false;
+        retractLazer = false;
+        StartCoroutine("fireCooldown");
+    }
+    private void Update()
+    {
+        if (isActive)
+        {
+
+            if (fireLazer && !retractLazer)
+            {
+                //moves lazer forwards and scales the lazer in the forward direction
+                lazerPrefab.transform.localScale += new Vector3(0, scaleFactor * Time.deltaTime, 0);
+                lazerPrefab.transform.localPosition += new Vector3(0, 0, scaleFactor * Time.deltaTime);
+
+                if (lazerPrefab.transform.lossyScale.y >= lazerLenght)
+                {
+                    retractLazer = true;
+                    warningPrefab.SetActive(false);
+                }
+            }
+            if (retractLazer)
+            {
+                //scaling down the lazer, whils tcontinuing movment in the forwards direction.
+                lazerPrefab.transform.localScale -= new Vector3(0, (scaleFactor * 2) * Time.deltaTime, 0);
+                lazerPrefab.transform.localPosition += new Vector3(0, 0, (scaleFactor * 2) * Time.deltaTime);
+
+                if (lazerPrefab.transform.lossyScale.y <= 0)
+                {
+                    disepateLazer();
+                }
+            }
+        }
+
+    }
+
+
     private void OnDrawGizmos()
     {
-        float raylenght = lazerLenght * 2 - 2.5f;
-        Gizmos.color = Color.red;
-        Gizmos.DrawRay(transform.position, transform.forward * raylenght);
+        if (drawRay)
+        {
+            float raylenght = lazerLenght * 200 + 100;
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(transform.position, transform.forward * raylenght);
+        }
+
     }
 }
--- Assets/Scripts/UI Scripts/Menue_Builder_Navigation.cs
+++ Assets/Scripts/UI Scripts/Menue_Builder_Navigation.cs
@@ -1,12 +1,148 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Menue_Builder_Navigation : MonoBehaviour
 {
-    
-    private void Update()
+
+    public RawImage activeImage, topImage, botImage;
+
+
+    public List<Texture> displayImage;
+    [Header("All available AT the current time")]
+    public List<int> allHazards;
+    //public List<string> displayText;
+
+    [HideInInspector]
+    public List<int> usableHazards;
+    //[HideInInspector]
+    public int currentDisplay;
+    private void Start()
     {
-        
+        currentDisplay = 0;
+        List<int> tempList = new List<int>();
+        tempList.Add(1);
+        tempList.Add(2);
+        tempList.Add(0);
+        TileChange(tempList);
+
+    }
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            DisplayChange(1);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            DisplayChange(-1);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            List<int> tempList = new List<int>();
+            tempList.Add(1);
+            //tempList.Add(2);
+            tempList.Add(0);
+            TileChange(tempList);
+        }
+    }
+    public void TileChange(List<int> inputHazards)
+    {
+        usableHazards = new List<int>();
+        foreach (int i in allHazards)
+        {
+            if (inputHazards.Contains(i))
+                usableHazards.Add(i);
+        }
+        //if a player cant place anything this is where the error will occur. 
+        DisplayChange(0);
+    }
+    public int DisplayChange(int changeValue)
+    {
+        currentDisplay = ValueCheck(currentDisplay, changeValue);
+
+        activeImage.texture = displayImage[usableHazards[currentDisplay]];
+        topImage.texture = displayImage[usableHazards[ValueCheck(currentDisplay, 1)]];
+        botImage.texture = displayImage[usableHazards[ValueCheck(currentDisplay, -1)]];
+
+        return usableHazards[currentDisplay];
+    }
+    int ValueCheck(int currentValue, int changeValue)
+    {
+        if (usableHazards.Count == 0)
+        {
+            Debug.LogError("available hazards is empty");
+            return 0;
+        }
+        currentValue += changeValue;
+
+        if (currentValue >= usableHazards.Count) currentValue = 0;
+        if (currentValue < 0) currentValue = usableHazards.Count - 1;
+
+        return currentValue;
     }
 }
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI;
+
+//public class Menue_Builder_Navigation : MonoBehaviour
+//{
+
+//    public RawImage activeImage, topImage, botImage;
+
+
+//    public List<int> returnValue;
+//    public List<Texture> displayImage;
+//    //public List<string> displayText;
+
+//    [HideInInspector]
+//    public List<int> avialbeHazards;
+//    //[HideInInspector]
+//    public int currentDisplay, topDisplay, botDisplay;
+//    private void Start()
+//    {
+//        currentDisplay = 0;
+//        topDisplay = 1;
+//        botDisplay = displayImage.Count - 1;
+//        DisplayChange(0);
+
+//    }
+//    public void Update()
+//    {
+//        if (Input.GetKeyDown(KeyCode.W))
+//        {
+//            DisplayChange(1);
+//        }
+//        if (Input.GetKeyDown(KeyCode.S))
+//        {
+//            DisplayChange(-1);
+//        }
+//    }
+//    public int DisplayChange(int changeValue)
+//    {
+//        currentDisplay += changeValue;
+//        topDisplay += changeValue;
+//        botDisplay += changeValue;
+
+//        if (currentDisplay >= displayImage.Count) currentDisplay = 0;
+//        if (topDisplay >= displayImage.Count) topDisplay = 0;
+//        if (botDisplay >= displayImage.Count) botDisplay = 0;
+
+
+//        if (currentDisplay < 0) currentDisplay = displayImage.Count - 1;
+//        if (topDisplay < 0) topDisplay = displayImage.Count - 1;
+//        if (botDisplay < 0) botDisplay = displayImage.Count - 1;
+
+
+//        activeImage.texture = displayImage[currentDisplay];
+//        topImage.texture = displayImage[topDisplay];
+//        botImage.texture = displayImage[botDisplay];
+
+
+
+
+//        return returnValue[currentDisplay];
+//    }
+//}
