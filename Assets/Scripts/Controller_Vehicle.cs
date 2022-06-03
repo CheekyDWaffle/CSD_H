765787902329fb7e8443627bca776ab57dad3579
Martin Talha <mart97@live.no>
2022-06-03 19:58:51 +0200

    Possibly fixed Joystick (I found a typo)

--- Assets/Scripts/Controller_Vehicle.cs
+++ Assets/Scripts/Controller_Vehicle.cs
@@ -188,7 +188,7 @@ public class Controller_Vehicle : MonoBehaviour
             KeyCode reverseKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), "Joystick" + currentIndex + "Button2");
 
             forwardModifier = (Input.GetKey(accelerationKey) ? 1 : 0) + (Input.GetKey(reverseKey) ? -0.1f : 0);
-            sidewayModifier = Input.GetAxis("Horizontal_C_" + currentIndex);
+            sidewayModifier = Input.GetAxis("Horizontal_C_" + (currentIndex-1));
 
             frictionModifier = Input.GetKey(handBrakeKey) ? driftModifier : 1f;
         }
