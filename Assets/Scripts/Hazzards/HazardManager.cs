54ae2b712a73e36154c1ad9c56746a57a95253d2
Sander <SaWIEn99@outlook.com>
2022-06-12 20:14:00 +0200

    Playtesting, Hazard ajustment and Lazer
    playtested, added lazer and did some hazard ajustments

--- Assets/Scripts/Hazzards/HazardManager.cs
+++ Assets/Scripts/Hazzards/HazardManager.cs
@@ -47,7 +47,7 @@ public class HazardManager : MonoBehaviour
                     RandomSpawn();
                     break;
                 case 2:
-
+                    ActivateHazard();
                     break;
                 default:
                     break;
